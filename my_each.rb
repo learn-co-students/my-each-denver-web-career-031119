ebfc6be36a8d60c1b1fbc40dc9d7859f1e32094c
sarahsakordaniels <ssdaniels13@gmail.com>
2019-03-12 07:39:36 -0600

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,10 @@
-def my_each # put argument(s) here
-  # code here
+
+array = [1 , 2, 3, 4]
+#I want my method to iterate over each number in the array and add one
+def my_each(array)
+
+while i < array.length
+  yield array[i]
+  i += 1
+end
 end
